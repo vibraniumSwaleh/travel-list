7427a9c379acf90d9f9f36acd03574a9ebd36b7b
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-11-30 23:35:53 +0000

    add main sections

---
+++ src/App.js
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trips?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className='list'>LIST</div>;
+}
+
+function Stats() {
+  <footer>
+    <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+  </footer>;
+}
+
+export default App;
