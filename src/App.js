791a191a20eb457bf7774dd3e434c079c8c5eb43
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-12-02 18:33:23 +0000

    add event handling functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -19,17 +21,34 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  function handleForm(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleForm}>
       <h3>What do you need for your 😍 trips?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-    </div>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
